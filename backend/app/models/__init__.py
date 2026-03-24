e5e9da1fcded13eff9fb8e011e3599297296ebb2
Putu Ngurah Semara <158034213+PutuNgurahSemara@users.noreply.github.com>
2026-03-24 13:24:52 +0800

    Delete backend directory

--- backend/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.models.asset import Asset, AssetStatus
-from app.models.borrow_log import BorrowLog
-from app.models.category import Category
-
-__all__ = ["Category", "Asset", "BorrowLog", "AssetStatus"]
